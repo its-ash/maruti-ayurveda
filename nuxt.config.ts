07b61fe12009e1acd46deb894ecf233a5e35815d
Ashvini Jangid <ashvini@functionize.com>
2025-10-11 10:41:03 +0530

    Update on github review

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,8 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
   modules: ["@nuxtjs/seo"],
+  ssr: false,
+  
   
   // SEO Configuration
   robots: {
